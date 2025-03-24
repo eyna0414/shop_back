cd59402b4b8e9d6aba1fc57781f1238b028016cc
eyna0414 <pey0414@gmail.com>
2025-03-24 17:44:53 +0900

    0324 book_shop 첨부파일기능

--- src/main/java/com/green/book_shop/book/controller/BookController.java
+++ src/main/java/com/green/book_shop/book/controller/BookController.java
@@ -2,26 +2,30 @@ package com.green.book_shop.book.controller;
 
 import com.green.book_shop.book.dto.BookDTO;
 import com.green.book_shop.book.service.BookService;
+import com.green.book_shop.utill.UploadUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/books")
 public class BookController {
   private final BookService bookService;
+  private final UploadUtil uploadUtil;
 
   //도서 등록 api
   //post ~/books
   @PostMapping("")
-  public void regBook(@RequestBody BookDTO bookDTO){
-    //첨부파일(도서 이미지) 업로드
+  public void regBook(BookDTO bookDTO, @RequestParam(name ="mainImg" , required = false) MultipartFile mainImg,
+                      @RequestParam(name ="subImg" , required = false) MultipartFile subImg
+  ){
 
+    //첨부파일(도서 이미지) 업로드
+    uploadUtil.fileUpload(mainImg);
+    uploadUtil.fileUpload(subImg);
     //BOOK 테이블에 데이터 INSERT
-    bookService.insertBook(bookDTO);
+    //bookService.insertBook(bookDTO);
   }
 
 }
